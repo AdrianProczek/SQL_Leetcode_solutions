0983fba273ca4f7f849eff9f430c8de29642c7b1
AdrianProczek <adrian.proczek@gmail.com>
2024-05-14 23:43:05 +0200

    Complete Task 595

--- SQL/595_Big_Countries.sql
+++ SQL/595_Big_Countries.sql
@@ -11,4 +11,4 @@
 -- name is the primary key (column with unique values) for this table.
 -- Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
 
-select name, population, area from world where area > 3000000 or population > 25000000
+select name, population, area from world where area >= 3000000 or population >= 25000000
