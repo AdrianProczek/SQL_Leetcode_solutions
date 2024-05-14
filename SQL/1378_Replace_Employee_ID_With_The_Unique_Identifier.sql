947218ea165a800916e0b2b606a026e9ff95c9eb
AdrianProczek <adrian.proczek@gmail.com>
2024-05-15 00:03:07 +0200

    Complete Task 1378

---
+++ SQL/1378_Replace_Employee_ID_With_The_Unique_Identifier.sql
@@ -0,0 +1,20 @@
+-- 1378. Replace Employee ID With The Unique Identifier
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the id and the name of an employee in a company.
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | unique_id     | int     |
+-- +---------------+---------+
+-- (id, unique_id) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+select EmployeeUNI.unique_id, Employees.name from Employees
+left join EmployeeUNI on Employees.id = EmployeeUNI.id
