0acb0abf29bdb2fc1ab65138a858cecdaa7303fb
AdrianProczek <adrian.proczek@gmail.com>
2024-05-14 23:13:02 +0200

    Complete Task 595

---
+++ SQL/595_Big_Countries.sql
@@ -0,0 +1,14 @@
+-- 595. Big Countries
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | name        | varchar |
+-- | continent   | varchar |
+-- | area        | int     |
+-- | population  | int     |
+-- | gdp         | bigint  |
+-- +-------------+---------+
+-- name is the primary key (column with unique values) for this table.
+-- Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+
+select name, population, area from world where area > 3000000 or population > 25000000
