700c4557312d3888b719e7ee80b5ab628568322e
AdrianProczek <adrian.proczek@gmail.com>
2024-05-15 00:57:10 +0200

    Complete Task 1661

---
+++ SQL/1661_Average_Time_of_Process_per_Machine.sql
@@ -0,0 +1,21 @@
+-- 1661. Average Time of Process per Machine
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | machine_id     | int     |
+-- | process_id     | int     |
+-- | activity_type  | enum    |
+-- | timestamp      | float   |
+-- +----------------+---------+
+-- The table shows the user activities for a factory website.
+-- (machine_id, process_id, activity_type) is the primary key (combination of columns with unique values) of this table.
+-- machine_id is the ID of a machine.
+-- process_id is the ID of a process running on the machine with ID machine_id.
+-- activity_type is an ENUM (category) of type ('start', 'end').
+-- timestamp is a float representing the current time in seconds.
+-- 'start' means the machine starts the process at the given timestamp and 'end' means the machine ends the process at the given timestamp.
+-- The 'start' timestamp will always be before the 'end' timestamp for every (machine_id, process_id) pair.
+
+select a.machine_id, round(avg(b.timestamp - a.timestamp), 3) as processing_time from Activity as a, Activity as b
+where a.machine_id = b.machine_id and a.process_id = b.process_id and a.activity_type = 'start' and b.activity_type = 'end'
+group by a.machine_id
