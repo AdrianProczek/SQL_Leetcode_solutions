b43896caafdddf65c748d3d641b7c2e3b099bdc7
AdrianProczek <adrian.proczek@gmail.com>
2024-05-14 23:53:18 +0200

    Complete Task 1683

---
+++ SQL/1683_Invalid_Tweets.sql
@@ -0,0 +1,11 @@
+-- 1683. Invalid Tweets
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | tweet_id       | int     |
+-- | content        | varchar |
+-- +----------------+---------+
+-- tweet_id is the primary key (column with unique values) for this table.
+-- This table contains all the tweets in a social media app.
+
+select tweet_id from tweets where length(content) > 15
