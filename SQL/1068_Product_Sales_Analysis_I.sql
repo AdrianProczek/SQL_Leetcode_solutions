790df34e48eba22a9df284e175a92f7fd8a0c3b9
AdrianProczek <adrian.proczek@gmail.com>
2024-05-15 00:09:24 +0200

    Complete Task 1068

---
+++ SQL/1068_Product_Sales_Analysis_I.sql
@@ -0,0 +1,25 @@
+-- 1068. Product Sales Analysis I
+-- +-------------+-------+
+-- | Column Name | Type  |
+-- +-------------+-------+
+-- | sale_id     | int   |
+-- | product_id  | int   |
+-- | year        | int   |
+-- | quantity    | int   |
+-- | price       | int   |
+-- +-------------+-------+
+-- (sale_id, year) is the primary key (combination of columns with unique values) of this table.
+-- product_id is a foreign key (reference column) to Product table.
+-- Each row of this table shows a sale on the product product_id in a certain year.
+-- Note that the price is per unit.
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | product_id   | int     |
+-- | product_name | varchar |
+-- +--------------+---------+
+-- product_id is the primary key (column with unique values) of this table.
+-- Each row of this table indicates the product name of each product.
+
+select Product.product_name, Sales.year, Sales.price from Sales
+left join Product on Product.product_id = Sales.product_id order by Sales.year desc
