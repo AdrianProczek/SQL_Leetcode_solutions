e6f0c6c8109617f5eab575563e3df2c8645b7af9
AdrianProczek <adrian.proczek@gmail.com>
2024-05-16 00:57:25 +0200

    Complete Task 1251

---
+++ SQL/1251_Average_Selling_Price.sql
@@ -0,0 +1,25 @@
+-- 1251. Average Selling Price
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | start_date    | date    |
+-- | end_date      | date    |
+-- | price         | int     |
+-- +---------------+---------+
+-- (product_id, start_date, end_date) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+-- For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | purchase_date | date    |
+-- | units         | int     |
+-- +---------------+---------+
+-- This table may contain duplicate rows.
+-- Each row of this table indicates the date, units, and product_id of each product sold. 
+
+select p.product_id, IFNULL(round(sum(price * units) / sum(units), 2), 0) as average_price from Prices as p
+left join UnitsSold as u on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
+group by product_id
