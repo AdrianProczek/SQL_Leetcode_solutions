7547fef7fc9aacb2e6c35a46e9b46e17f7d786fd
AdrianProczek <adrian.proczek@gmail.com>
2024-05-15 20:50:38 +0200

    Complete Task 570

---
+++ SQL/570_Managers_with_at_Least_5_Direct_Reports.sql
@@ -0,0 +1,16 @@
+-- 570. Managers with at Least 5 Direct Reports
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | department  | varchar |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the name of an employee, their department, and the id of their manager.
+-- If managerId is null, then the employee does not have a manager.
+-- No employee will be the manager of themself.
+
+select name from Employee as emp1
+where emp1.id in (select managerId from Employee as emp2 group by managerId having count(managerId) >= 5)
