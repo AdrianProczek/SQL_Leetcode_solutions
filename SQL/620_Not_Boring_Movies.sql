81d6c813932326e17ce76fd47d79d3506d758e39
AdrianProczek <adrian.proczek@gmail.com>
2024-05-15 23:19:49 +0200

    Complete Task 620

---
+++ SQL/620_Not_Boring_Movies.sql
@@ -0,0 +1,14 @@
+-- 620. Not Boring Movies
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | id             | int      |
+-- | movie          | varchar  |
+-- | description    | varchar  |
+-- | rating         | float    |
+-- +----------------+----------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row contains information about the name of a movie, its genre, and its rating.
+-- rating is a 2 decimal places float in the range [0, 10]
+
+select * from cinema where id % 2 != 0 and description != 'boring' order by rating desc
