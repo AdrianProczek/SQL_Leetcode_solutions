d9249aacf66dba06555e0f52189a4031ad4e9e4a
AdrianProczek <adrian.proczek@gmail.com>
2024-05-16 23:11:39 +0200

    Complete Task 1075

---
+++ SQL/1075_Project_Employees_I.sql
@@ -0,0 +1,23 @@
+-- 1075. Project Employees I
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | project_id  | int     |
+-- | employee_id | int     |
+-- +-------------+---------+
+-- (project_id, employee_id) is the primary key of this table.
+-- employee_id is a foreign key to Employee table.
+-- Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+-- +------------------+---------+
+-- | Column Name      | Type    |
+-- +------------------+---------+
+-- | employee_id      | int     |
+-- | name             | varchar |
+-- | experience_years | int     |
+-- +------------------+---------+
+-- employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
+-- Each row of this table contains information about one employee.
+
+select p.project_id, round(sum(e.experience_years) / count(e.experience_years), 2) as average_years from Project as p
+left join Employee as e on p.employee_id = e.employee_id
+group by p.project_id
