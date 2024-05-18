4969371d5d7caca191df3218febbc5b7ce953492
AdrianProczek <adrian.proczek@gmail.com>
2024-05-18 17:10:22 +0200

    Complete Task 2356

---
+++ SQL/2356_Number_of_Unique_Subjects_Taught_by_Each_Teacher.sql
@@ -0,0 +1,13 @@
+-- 2356. Number of Unique Subjects Taught by Each Teacher
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | teacher_id  | int  |
+-- | subject_id  | int  |
+-- | dept_id     | int  |
+-- +-------------+------+
+-- (subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+-- Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+
+select teacher_id, count(distinct subject_id) as cnt
+from Teacher group by teacher_id
