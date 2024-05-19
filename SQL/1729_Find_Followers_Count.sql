6d66766091dac8fe1dd735726c83ab212215d21d
AdrianProczek <adrian.proczek@gmail.com>
2024-05-19 17:58:37 +0200

    Complete Task 1729

---
+++ SQL/1729_Find_Followers_Count.sql
@@ -0,0 +1,14 @@
+-- 1729. Find Followers Count
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | user_id     | int  |
+-- | follower_id | int  |
+-- +-------------+------+
+-- (user_id, follower_id) is the primary key (combination of columns with unique values) for this table.
+-- This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+
+select user_id, count(follower_id) as followers_count
+from Followers
+group by user_id
+order by user_id
