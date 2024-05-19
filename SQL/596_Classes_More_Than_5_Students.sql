72dfc77bfbd3fa5677197f1402ca1134fcbb92b5
AdrianProczek <adrian.proczek@gmail.com>
2024-05-19 17:56:11 +0200

    Complete Task 596

---
+++ SQL/596_Classes_More_Than_5_Students.sql
@@ -0,0 +1,11 @@
+-- 596. Classes More Than 5 Students
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | student     | varchar |
+-- | class       | varchar |
+-- +-------------+---------+
+-- (student, class) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table indicates the name of a student and the class in which they are enrolled.
+
+select class from Courses group by class having count(student) >= 5
