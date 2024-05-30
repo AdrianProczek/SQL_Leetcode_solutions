dd7146cbe21087a2cb549e301d3fbc5a5433fbf2
AdrianProczek <adrian.proczek@gmail.com>
2024-05-30 23:22:31 +0200

    Complete Task 176

---
+++ Medium/176_Second_Highest_Salary.sql
@@ -0,0 +1,56 @@
+-- 176. Second Highest Salary
+-- Table: Employee
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | salary      | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains information about the salary of an employee.
+
+ 
+
+-- Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+
+-- Example 2:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | null                |
+-- +---------------------+
+
+select max(e1.salary) as SecondHighestSalary from Employee as e1 
+where e1.salary < (select max(e2.salary) from Employee as e2)
