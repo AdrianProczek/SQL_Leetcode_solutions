6649d6dce9313cf1e653b96b68606efa1026f037
AdrianProczek <adrian.proczek@gmail.com>
2024-06-26 17:21:44 +0200

    Complete Task 586

---
+++ Easy/586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -0,0 +1,46 @@
+-- 586. Customer Placing the Largest Number of Orders
+-- Table: Orders
+
+-- +-----------------+----------+
+-- | Column Name     | Type     |
+-- +-----------------+----------+
+-- | order_number    | int      |
+-- | customer_number | int      |
+-- +-----------------+----------+
+-- order_number is the primary key (column with unique values) for this table.
+-- This table contains information about the order ID and the customer ID.
+
+ 
+
+-- Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+
+-- The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Orders table:
+-- +--------------+-----------------+
+-- | order_number | customer_number |
+-- +--------------+-----------------+
+-- | 1            | 1               |
+-- | 2            | 2               |
+-- | 3            | 3               |
+-- | 4            | 3               |
+-- +--------------+-----------------+
+-- Output: 
+-- +-----------------+
+-- | customer_number |
+-- +-----------------+
+-- | 3               |
+-- +-----------------+
+-- Explanation: 
+-- The customer with number 3 has two orders, which is greater than either customer 1 or 2 because each of them only has one order. 
+-- So the result is customer_number 3.
+
+select top 1 customer_number from orders
+group by customer_number order by count(order_number) desc
